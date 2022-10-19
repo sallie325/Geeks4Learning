a152093b0963f13f8e36487a45df68bc7f6c505f
Sinovuyo97 <sinovuyoshakes@gmail.com>
2022-10-19 12:04:42 +0200

    pushing up code

--- G4L.UserManagement.UI/src/app/leave-management/total-pending-approval-rejection/total-pending-approval-rejection.component.ts
+++ G4L.UserManagement.UI/src/app/leave-management/total-pending-approval-rejection/total-pending-approval-rejection.component.ts
@@ -8,34 +8,40 @@ import { LeaveService } from '../services/leave.service';
 })
 export class TotalPendingApprovalRejectionComponent implements OnInit {
   allRequests: any;
-  pending: any = 0;
-  approved: any = 0;
-  rejected: any = 0;
+  pending: number = 0;
+  approved: number = 0;
+  rejected: number = 0;
+  cancelled: number = 0;
   constructor(private _leaveService: LeaveService) { }
 
   ngOnInit() {
     this.getAllLeaveApplications();
   }
-  calcStatuses(status: any) {
-    // if (status == "Pending") {
-    //   this.pending += 1;
-    // } else if (status.equals("Approved")) {
-    //   this.approved += 1;
-    // } else if (status.equals("Rejected")) {
-    //   this.rejected += 1;
-    // }
-  }
   getAllLeaveApplications() {
     this._leaveService.getAllLeaveApplications().subscribe((requests) => {
       this.allRequests = requests;
       this.allRequests.forEach((x: any) => {
-        if (x.status == "Cancelled") {
+        if (x.status == "Approved") {
           this.approved += 1;
-        } else {
-          this.pending +=1;
+          if(this.pending > 0){
+            this.pending -=1;
+          }
+          
+        } else if (x.status == "Pending") {
+          this.pending += 1;
+        }
+        else if (x.status == "Rejected") {
+          this.rejected += 1;
+          if(this.pending > 0){
+            this.pending -=1;
+          }
+        } else if (x.status == "Cancelled") {
+          this.cancelled += 1;
+          if(this.pending > 0){
+            this.pending -=1;
+          }
         }
         console.log(this.allRequests);
-        this.calcStatuses(x.status);
       })
     })
   }
