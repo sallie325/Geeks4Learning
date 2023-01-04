40c46e0b1da14e3f51a8ab000316fade62489aa9
Tshimangadzo Mudzanani <mudzanani27@gmail.com>
2023-01-04 12:50:18 +0200

    added sponsor to leave request model

--- G4L.UserManagement.BL/Models/Request/LeaveRequest.cs
+++ G4L.UserManagement.BL/Models/Request/LeaveRequest.cs
@@ -3,9 +3,6 @@ using G4L.UserManagement.BL.Enum;
 using G4L.UserManagement.BL.Models.Response;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace G4L.UserManagement.BL.Models.Request
 {
@@ -14,16 +11,17 @@ namespace G4L.UserManagement.BL.Models.Request
         public Guid Id { get; set; }
         public Guid UserId { get; set; }
         public User User { get; set; }
+        public SponsorRequest Sponsor { get; set; }
         public List<LeaveBalanceResponse> LeaveBalances { get; set; }
         public LeaveType LeaveType { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public decimal UsedDays { get; set; }
         public List<LeaveScheduleRequest> LeaveSchedule { get; set; }
-        //Itumeleng added this
         public string Comments { get; set; }
         public Status Status { get; set; } = Status.Pending;
         public ICollection<ApproverRequest> Approvers { get; set; }
         public ICollection<DocumentRequest> Documents { get; set; }  // TODO: think of a better way to save attachments
+   
     }
 }
