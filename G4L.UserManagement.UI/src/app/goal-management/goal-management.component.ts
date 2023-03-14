9688d099ae3c7ed0d3e2662bd0df8fd1845bb384
Nhlamulo <u15231748@tuks.co.za>
2023-03-14 20:28:22 +0200

    Added validation for the gaols

--- G4L.UserManagement.UI/src/app/goal-management/goal-management.component.ts
+++ G4L.UserManagement.UI/src/app/goal-management/goal-management.component.ts
@@ -1,71 +1,118 @@
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
-import { GoalModel } from './models/goal-model';
+import { GoalModel, goalTypes } from './models/goal-model';
 
 @Component({
-  selector: 'app-goal-management',
-  templateUrl: './goal-management.component.html',
-  styleUrls: ['./goal-management.component.css']
+	selector: 'app-goal-management',
+	templateUrl: './goal-management.component.html',
+	styleUrls: ['./goal-management.component.css']
 })
 export class GoalManagementComponent {
-  _backlog: Array<GoalModel> = [
-    {
-      title: "Begin OOP Classes",
-      description: "I want to start with basics and then build up from there...",
-      duration: "2 Hours"
-    },
-    {
-      title: "Learn Springboot",
-      description: "Will start with the basis of springboot today",
-      duration: "2 Hours"
-    },
-  ]
+	// Goal states
+	backlogState: goalTypes = "backlog"
+	startedState: goalTypes = "started"
+	pausedState: goalTypes = "paused"
+	completedState: goalTypes = "completed"
+	archivedState: goalTypes = "archived"
 
-  _paused: Array<GoalModel> = [
-    {
-      title: "Solve a Binary Search Algorithm",
-      description: "I want to test my understanding of the algorithm",
-      duration: "1 Hour"
-    },
-    {
-      title: "Go to pick N pay",
-      description: "Need something to spice-up my lunch",
-      duration: "1 Hour"
-    },
-  ]
+	_backlog: Array<GoalModel> = [
+		{
+			id: 1,
+			title: "Begin OOP Classes",
+			description: "I want to start with basics and then build up from there...",
+			duration: "2 Hours",
+			pausedCount: 0,
+			tasks: [
+				{
+					id: 1,
+					title: "Setup my environment",
+					complete: false
+				}
+			]
+		},
+		{
+			id: 2,
+			title: "Learn Springboot",
+			description: "Will start with the basis of springboot today",
+			duration: "2 Hours",
+			pausedCount: 0,
+		},
+	]
 
-  _archived: Array<GoalModel> = [
-    {
-      title: "Solve a Binary Search Algorithm",
-      description: "I want to test my understanding of the algorithm",
-      duration: "1 Hour"
-    },
-    {
-      title: "Go to pick N pay",
-      description: "Need something to spice-up my lunch",
-      duration: "1 Hour"
-    },
-  ]
+	_paused: Array<GoalModel> = [
+		{
+			id: 1,
+			title: "Solve a Binary Search Algorithm",
+			description: "I want to test my understanding of the algorithm",
+			duration: "1 Hour",
+			pausedCount: 0,
+		},
+		{
+			id: 2,
+			title: "Do a 5 Minute Walk",
+			description: "Need this for mind refreshment",
+			duration: "1 Hour",
+			pausedCount: 0,
+		},
+	]
 
-  _started: Array<GoalModel> = []
-  _completed: Array<GoalModel> = []
+	_archived: Array<GoalModel> = [
+		{
+			id: 1,
+			title: "Solve a Binary Search Algorithm",
+			description: "I want to test my understanding of the algorithm",
+			duration: "1 Hour",
+			pausedCount: 0,
+		},
+		{
+			id: 2,
+			title: "Go to pick N pay",
+			description: "Need something to spice-up my lunch",
+			duration: "30min",
+			pausedCount: 0,
+		},
+	]
 
-  constructor() { }
+	_started: Array<GoalModel> = []
+	_completed: Array<GoalModel> = []
 
-  onDropGoal = (event: CdkDragDrop<Array<any>>) => {
-    if (event.previousContainer === event.container) {
-      moveItemInArray(
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex
-      );
-    } else {
-      transferArrayItem(
-        event.previousContainer.data,
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex,
-      );
-    }
-  }
+	constructor() { }
+
+	onDropGoal = (event: CdkDragDrop<Array<any>>): void => {
+		if (event.previousContainer === event.container) {
+			moveItemInArray(
+				event.container.data,
+				event.previousIndex,
+				event.currentIndex
+			);
+		} else {
+			const previousContainerLinks: Array<string> = event.previousContainer.connectedTo.toString().split(',')
+
+			// Checking if the current card is movable to the target container
+			if (previousContainerLinks.includes(event.container.id)) {
+				// Handling Goal Activity Logic
+				switch (event.container.id) {
+					case this.archivedState:
+						const response = prompt("Why are you archieving this goal?")
+						if (response === null) return;
+						alert(response)
+						break;
+					case this.pausedState:
+						if(this._started[event.previousIndex].pausedCount === 5){
+							alert("This goal cannot be paused any longer, you must complete it!")
+							return;
+						}
+						this._started[event.previousIndex].pausedCount += 1;
+						break;
+				}
+
+				transferArrayItem(
+					event.previousContainer.data,
+					event.container.data,
+					event.previousIndex,
+					event.currentIndex,
+				);
+			}
+		}
+	}
 }
--- G4L.UserManagement.UI/src/app/goal-management/models/goal-model.ts
+++ G4L.UserManagement.UI/src/app/goal-management/models/goal-model.ts
@@ -1,7 +1,17 @@
 export type goalTypes = "backlog" | "started" | "paused" | "completed" | "archived"
 
+export interface GoalTaskModel {
+    id?: number,
+    title: string,
+    complete: boolean
+}
+
 export interface GoalModel {
+    id?: number,
     title: string,
     description: string,
-    duration: string
+    duration: string,
+    tasks?: Array<GoalTaskModel>,
+    comment?: string,
+    pausedCount: number
 }
