b77c42fc16549b7e0340937da783ccca097ab37f
Tshimangadzo <mudzanani27@gmail.com>
2023-03-15 08:21:41 +0200

    Stash

--- G4L.UserManagement.UI/src/app/goal-management/views/goal-card/goal-card.component.ts
+++ G4L.UserManagement.UI/src/app/goal-management/views/goal-card/goal-card.component.ts
@@ -1,9 +1,12 @@
 import { Component, Input } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ViewSelectedGoalComponent } from '../../modals/views/view-selected-goal/view-selected-goal.component';
+import { GoalModel } from '../../models/goal-model';
 
 @Component({
   selector: 'app-goal-card',
   templateUrl: './goal-card.component.html',
-  styleUrls: ['./goal-card.component.css']
+  styleUrls: ['./goal-card.component.css'],
 })
 export class GoalCardComponent {
   @Input()
@@ -16,17 +19,34 @@ export class GoalCardComponent {
   goalDuration!: string;
 
   @Input()
-  goalState!: "backlog" | "started" | "paused" | "completed" | "archived"
+  goalState!: 'backlog' | 'started' | 'paused' | 'completed' | 'archived';
+  GoalModel!: GoalModel;
+  name: any;
+  animal: any;
 
-  constructor() { }
+  constructor(public dialog: MatDialog) {}
 
   grab(event: any) {
-    const { target } = event
-    target.style.cursor = 'grabbing'
+    const { target } = event;
+    target.style.cursor = 'grabbing';
   }
 
   release(event: any) {
-    const { target } = event
-    target.style.cursor = 'grab'
+    const { target } = event;
+    target.style.cursor = 'grab';
+  }
+  onOpenDialog(): void {
+    const dialogRef = this.dialog.open(ViewSelectedGoalComponent, {
+      data: {
+        title: this.goalTitle,
+        duration: this.goalDuration,
+        description: this.goalDescription,
+      },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+      this.goalDescription = result;
+    });
   }
 }
