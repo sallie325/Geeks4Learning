48dd288b1f54a5fde4e2f1243cac89667d524faf
SaneManiacMG <mmoroane@hotmail.com>
2023-03-29 08:06:16 +0200

    Updated Migrations

--- G4L.UserManagement.BL/Models/Response/GoalList.cs
+++ G4L.UserManagement.BL/Models/Response/GoalList.cs
@@ -1,15 +1,16 @@
-﻿using G4L.UserManagement.BL.Enum;
-using G4L.UserManagement.Shared;
+﻿using G4L.UserManagement.BL.Entities;
+using G4L.UserManagement.BL.Enum;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace G4L.UserManagement.BL.Entities
+namespace G4L.UserManagement.BL.Models.Response
 {
-    public class Goal: BaseEntity
+    public class GoalList
     {
+        public Guid GoalId { get; set; }
         public string GoalTitle { get; set; }
         public string Description { get; set; }
         public TimeSpan Duration { get; set; }
@@ -18,8 +19,8 @@ namespace G4L.UserManagement.BL.Entities
         public int ArchiveCount { get; set; }
         public Guid UserId { get; set; }
         public Guid AttendenceId { get; set; }
-        public List<GoalComment> Comments { get; set; }
+        public List<GoalCommentResponse> Comments { get; set; }
         public TimeSpan TimeLeft { get; set; }
-        public List<GoalTask> GoalTasks { get; set; }
+        public List<GoalTaskResponse> GoalTasks { get; set; }
     }
 }
