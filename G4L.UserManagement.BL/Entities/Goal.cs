1fd945dea5cddab8750b56b1c1f1016685b57eb1
Thapelo Seema <thapelo.seema@geeks4learning.com>
2023-03-29 04:08:26 +0200

    Aligned Goal Entity and removed User property of Goal

--- G4L.UserManagement.BL/Entities/Goal.cs
+++ G4L.UserManagement.BL/Entities/Goal.cs
@@ -14,7 +14,7 @@ namespace G4L.UserManagement.BL.Entities
         public Guid GoalId { get; set; } //Maybe we can have just int ids for goals
         public string Title { get; set; }
         public string Description { get; set; }
-<<<<<<< HEAD
+
         public TimeSpan Duration { get; set; } //The name is misleading, we should store this as DateTime named EndTime/ExpiryTime
         public GoalStatus GoalStatus { get; set; }
         public int PauseCount { get; set; }
@@ -29,17 +29,6 @@ namespace G4L.UserManagement.BL.Entities
             goalTasks = new List<GoalTask>();  
             GoalId = Guid.NewGuid();
         }
-=======
-        public TimeSpan Duration { get; set; }
-        public int PausedCount { get; set; }
-        public int ArchiveCount { get; set; }
-        public GoalStatus GoalStatus { get; set; }
-        public TimeSpan TimeRemaining { get; set; }
-        public List<GoalComment> Comment { get; set; }
-        public List<GoalTask> Tasks { get; set; }
-        public Guid AttendenceId { get; set; }
-        public Guid UserId { get; set; }
-        public User User { get; set; }
->>>>>>> 7d8114f0afb594182dfccb5ae89e2187b7d9c65f
+
     }
 }
